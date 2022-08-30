94422b5656742e85a2af0a9702b84b18da19f18d
Je Ar Becerro <jearbecerro@gmail.com>
2022-08-30 11:20:33 +0800

    test

--- DynamicSearch.js
+++ DynamicSearch.js
@@ -8,7 +8,7 @@ export function DynamicSearch(toSearch, list){
             const key = keys[i]
             var value = values[i]
             value = Array.isArray(value)? value : [value]
-            if(value!==""){//
+            if(value!==""){
                 query.push({
                         Field: [key], Values: value
                 })
@@ -16,23 +16,29 @@ export function DynamicSearch(toSearch, list){
         }
         console.log(query)
         Array.prototype.flexFilter = function(query) {
-          var matchesFilter, matches = [], count;
-          matchesFilter = function(item) {
-            count = 0
-            for (var n = 0; n < query.length; n++) {
-              if ( query[n]["Values"].indexOf(item[query[n]["Field"]]) > -1){
-                count++;
+  
+            var matchesFilter, matches = [], count;
+            
+            matchesFilter = function(item) {
+              count = 0
+              for (var n = 0; n < query.length; n++) {
+                if (query[n]["Values"].indexOf(item[query[n]["Field"]]) > -1) {
+                  count++;
+                }
+              }
+
+              return count === query.length;
+            }
+          
+            for (var i = 0; i < this.length; i++) {
+              if (matchesFilter(this[i])) {
+                matches.push(this[i]);
               }
             }
-            return count === query.length;
+          
+            return matches;
           }
-          for (var i = 0; i < this.length; i++) {
-            if (matchesFilter(this[i])) {
-              matches.push(this[i]);
-            }
-          }
-          return matches;
-        }
+        console.log("was here")
         console.log(list.flexFilter(query))
         return list.flexFilter(query)      
       } catch (err) {
